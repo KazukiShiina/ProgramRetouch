963e93294ac62492b5ffd169bdd0cf46523e3bfe
椎名和貴 <c7mail.com@gmail.com>
2019-01-31 13:47:47 +0900

    all

---
+++ EC/src/ec/UserBuyHistoryDetail.java
@@ -0,0 +1,79 @@
+package ec;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.BuyDataBeans;
+import beans.ItemDataBeans;
+import beans.UserDataBeans;
+import dao.BuyDAO;
+import dao.BuyDetailDAO;
+import dao.UserDAO;
+
+/**
+ * 購入履歴画面
+ * @author d-yamaguchi
+ *
+ */
+@WebServlet("/UserBuyHistoryDetail")
+public class UserBuyHistoryDetail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// セッション開始
+		HttpSession session = request.getSession();
+		try {
+			// ログイン時に取得したユーザーIDをセッションから取得
+			int userId = (int) session.getAttribute("userId");
+			// 更新確認画面から戻ってきた場合Sessionから取得。それ以外はuserIdでユーザーを取得
+			UserDataBeans udb = session.getAttribute("returnUDB") == null ? UserDAO.getUserDataBeansByUserId(userId) : (UserDataBeans) EcHelper.cutSessionAttribute(session, "returnUDB");
+
+
+
+			// 入力された内容に誤りがあったとき等に表示するエラーメッセージを格納する
+			String validationMessage = (String) EcHelper.cutSessionAttribute(session, "validationMessage");
+
+			String id  = (String) request.getParameter("buy_id");
+			System.out.println(id);
+			BuyDAO buyDao = new BuyDAO();
+			BuyDetailDAO bdDao = new BuyDetailDAO();
+
+			int i = Integer.parseInt(id);
+
+			BuyDataBeans buyId = buyDao.getBuyDataBeansByBuyId(i);
+			ArrayList<ItemDataBeans> buyInfo = bdDao.getItemDataBeansListByBuyId(i);
+
+			System.out.println(i+"だお");
+
+
+			request.setAttribute("validationMessage", validationMessage);
+			request.setAttribute("udb", udb);
+			request.setAttribute("buyId", buyId);
+			request.setAttribute("buyInfo", buyInfo);
+			System.out.println(buyId);
+			System.out.println(buyInfo);
+
+			request.getRequestDispatcher(EcHelper.USER_BUY_HISTORY_DETAIL_PAGE).forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("errorMessage", e.toString());
+			response.sendRedirect("Error");
+		}
+	}
+
+}
+
+
+
+
+
+
